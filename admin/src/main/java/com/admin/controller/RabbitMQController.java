96e8858b1c315f448452a6c5d6fa78912787354a
wang <91462854+JiaLiDunK@users.noreply.github.com>
2025-11-28 16:03:34 +0800

    引入了langchain的写法

--- admin/src/main/java/com/admin/controller/RabbitMQController.java
+++ admin/src/main/java/com/admin/controller/RabbitMQController.java
@@ -1,13 +1,19 @@
 package com.admin.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/rabbit")
 public class RabbitMQController {
+    @Value("${tongyi.model.qwenPlus}")
+    private String chat;
     @RequestMapping("/test")
     public String test() {
+        log.info("看这个:{}",chat);
         return "测试";
     }
 }
--- agent/src/main/java/com/agent/config/LangChain4jChatConfig.java
+++ agent/src/main/java/com/agent/config/LangChain4jChatConfig.java
@@ -3,12 +3,25 @@ package com.agent.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import dev.langchain4j.model.openai.OpenAiChatModel;
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "openai")
 public class LangChain4jChatConfig {
-    @Value("${openai.chat.base-url}")
+    @Value("${tongyi.base-url}")
     private String baseUrl;
+    @Value("${tongyi.model:qwenPlus}")
+    private String model;
+    @Value("${tongyi.api-key}")
+    private String apiKey;
+    @Bean
+    OpenAiChatModel qwenPlusModel(){
+        return OpenAiChatModel.builder()
+                .baseUrl(baseUrl)
+                .modelName(model)
+                .apiKey(apiKey)
+                .build();
+    }
 }
